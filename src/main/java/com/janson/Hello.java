ed48912a35f2a1d3c07e5172cda7f02dc56d5850
Janson <janson04@gmail.com>
2022-02-21 11:52:26 +0800

    20220221Change

--- src/main/java/com/janson/Hello.java
+++ src/main/java/com/janson/Hello.java
@@ -4,5 +4,7 @@ public class Hello {
     public static void main(String[] args) {
         Person p=new Person();
         p.hello();
+        byte a = 127;
+        
     }
 }
--- src/main/kotlin/com/janson/Hello.kt
+++ src/main/kotlin/com/janson/Hello.kt
@@ -5,4 +5,6 @@ fun main() {
 //    Human().hello()
     val h=Human();
     h.hello();
+    var age:Int = 100;
+    age = 11;
 }
