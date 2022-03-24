da0e1d70cc95cc3398e079eb194904f1916d8dc9
janson04 <janson04@gmail.com>
2022-03-24 08:00:01 +0800

    20220324_P1

--- src/main/kotlin/com/janson/Ticket.kt
+++ src/main/kotlin/com/janson/Ticket.kt
@@ -1,7 +1,22 @@
 package com.janson.kotlin
 
+import java.util.Random
+
+
 data class Ticket(val origin:Int, val destination:Int , val price:Int)
 
 fun main() {
     var ticket = Ticket(20, 51, 420)
+    val s = "abcdefg"
+    println(s.validata())
+    println((1..10).random())
+}
+
+fun String.validata() : Boolean {
+    return this.length >= 6
+}
+
+fun IntRange.random(): Int {
+    val r = Random().nextInt(endInclusive-start) + start;
+    return r
 }
