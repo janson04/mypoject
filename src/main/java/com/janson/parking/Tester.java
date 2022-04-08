22406147a58f3c6ee1f9a85919582aacc24e7450
janson04 <janson04@gmail.com>
2022-04-09 07:14:14 +0800

    20220409_P1

--- src/main/java/com/janson/parking/Tester.java
+++ src/main/java/com/janson/parking/Tester.java
@@ -1,6 +1,8 @@
 package com.janson.parking;
 
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 
 public class Tester {
@@ -10,5 +12,20 @@ public class Tester {
         System.out.println(date.getTime());
         SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
         System.out.println(sdf.format(date));
+        String s = "2022/4/25 08:05:11";
+        Date otherday = null;
+        try {
+            otherday = sdf.parse(s);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        System.out.println(otherday);
+
+        Calendar calendar = Calendar.getInstance();
+        System.out.println(calendar.getTime());
+        calendar.set(Calendar.MONTH,9);
+        System.out.println(calendar.getTime());
+        calendar.add(Calendar.DATE,3);
+        System.out.println(calendar.getTime());
     }
 }
